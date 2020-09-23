bac23ab29da18ef027937fd2c21f28e470c20dd8
anurag <anurag@pop-os.localdomain>
2020-09-23 22:12:40 +0530

    Changed Build Folders

--- config.php
+++ config.php
@@ -2,6 +2,10 @@
 
 return [
     'production' => true,
+    'build' => [
+        'source' => 'source',
+        'destination' => 'docs',
+    ],
     'baseUrl' => '',
     'title' => 'Freelancer - Anurag Makol',
     'description' => 'Website description.',
